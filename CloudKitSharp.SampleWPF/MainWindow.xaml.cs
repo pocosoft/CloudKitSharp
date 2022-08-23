e97ae9bc518a061b2987427e57c75e318b8b0d52
Akihiro Orikasa <a.orikasa@pocosoft.jp>
2022-08-23 12:11:24 +0900

    Get web auth token.

--- CloudKitSharp.SampleWPF/MainWindow.xaml.cs
+++ CloudKitSharp.SampleWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,20 +22,41 @@ namespace CloudKitSharp.SampleWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string? _webAuthToken;
+        private readonly CKClient _client = new CKClient(
+                "iCloud.jp.pocosoft.CloudKitSharp",
+                "70b43599849b12f90464938ed1aed59b999c2ee18e9cd3936a18e15b49f584d8",
+                "b2eb419a4ab40758d32657a0d07cf84d44553891590095b9d89fa674d9b8a650"
+            );
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var ckClient = new CKClient(
-                "iCloud.jp.pocosoft.CloudKitSharp",
-                "70b43599849b12f90464938ed1aed59b999c2ee18e9cd3936a18e15b49f584d8",
-                "b2eb419a4ab40758d32657a0d07cf84d44553891590095b9d89fa674d9b8a650"
-                );
-            var response = await ckClient.GetUsersCaller(null);
+            GetWebAuthToken(_webAuthToken);
+        }
+        private void GetWebAuthToken(string webAuthToken)
+        {
+            Debug.Print(webAuthToken);
+            _webAuthToken = webAuthToken;
+            GetUserCaller(_webAuthToken);
+        }
+
+        private async void GetUserCaller(string? webAuthToken)
+        {
+            var response = await _client.GetUsersCaller(webAuthToken);
             DebugConsole.Text = response.Content;
+
+            if (!response.IsSuccessful && response.Content != null)
+            {
+                CKError error = CKError.Parse(response.Content);
+                Debug.Print(error.RedirectURL);
+                var web = new WebViewWinow(new Uri(error.RedirectURL));
+                web.GetWebAuthTokenDelegate += GetWebAuthToken;
+                web.ShowDialog();
+            }
         }
     }
 }
---
+++ CloudKitSharp.SampleWPF/WebViewWinow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CloudKitSharp.SampleWPF
+{
+    /// <summary>
+    /// WebViewWinow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class WebViewWinow : Window
+    {
+        public Action<string> GetWebAuthTokenDelegate;
+        public Uri Source;
+        public WebViewWinow(Uri source)
+        {
+            InitializeComponent();
+            Source = source;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WebView.Source = Source;
+        }
+
+        private void WebView_NavigationStarting(
+            object sender, 
+            Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
+        {
+            if (e.Uri.StartsWith("http://localhost"))
+            {
+                var uri = new Uri(e.Uri);
+                var query = HttpUtility.ParseQueryString(uri.Query);
+                var webAuthToken = query.Get("ckWebAuthToken");
+                if (webAuthToken != null)
+                {
+                    GetWebAuthTokenDelegate(webAuthToken);
+                    Close();
+                }
+            }
+        }
+    }
+}
