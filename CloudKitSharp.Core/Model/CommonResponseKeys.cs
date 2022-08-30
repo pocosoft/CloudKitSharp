ed3d8e5a3eda54bc66076e655f59ba83ae2626df
Akihiro Orikasa <a.orikasa@pocosoft.jp>
2022-08-30 17:54:01 +0900

    Change to TextBox

---
+++ CloudKitSharp.Core/Model/CommonResponseKeys.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudKitSharp.Core.Model
+{
+    /// <summary>
+    /// Common Response Keys
+    /// These record keys appear in responses:
+    /// </summary>
+    /// <see cref=""/>
+    public abstract class CommonResponseKeys
+    {
+
+    }
+}
