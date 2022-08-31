90ba5812b2b434df30fbe23e574a14a92e6d84bc
Akihiro Orikasa <a.orikasa@pocosoft.jp>
2022-09-01 03:31:21 +0900

    Modify-request

--- CloudKitSharp.Core/Http/RecordsModifyResponse.cs
+++ CloudKitSharp.Core/Http/RecordsModifyResponse.cs
@@ -7,19 +7,6 @@ namespace CloudKitSharp.Core.Http
     /// Modifying Records (records/modify)
     /// </summary>
     /// <see cref="https://developer.apple.com/library/archive/documentation/DataManagement/Conceptual/CloudKitWebServicesReference/ModifyRecords.html#//apple_ref/doc/uid/TP40015240-CH2-SW9"/>
-    public class RecordsModifyResponse
-    {
-        /// <summary>
-        /// An array containing a result dictionary for each operation in the request. If successful, the result dictionary contains the keys described in Record Dictionary. If unsuccessful, the result dictionary contains the keys described in Record Fetch Error Dictionary.
-        /// </summary>
-        [JsonPropertyName("records")]
-        public List<RecordDictionary>? records { get; set; }
-    }
-
-    /// <summary>
-    /// Modifying Records (records/modify)
-    /// </summary>
-    /// <see cref="https://developer.apple.com/library/archive/documentation/DataManagement/Conceptual/CloudKitWebServicesReference/ModifyRecords.html#//apple_ref/doc/uid/TP40015240-CH2-SW9"/>
     public class RecordsModifyResponse<T>
     {
         /// <summary>
--- CloudKitSharp.Core/Http/RecordsQueryResponse.cs
+++ CloudKitSharp.Core/Http/RecordsQueryResponse.cs
@@ -4,12 +4,6 @@ using System.Text.Json.Serialization;
 
 namespace CloudKitSharp.Core.Http
 {
-    public class RecordsQueryResponse
-    {
-        [JsonPropertyName("records")]
-        public List<RecordDictionary>? records { get; set; }
-    }
-
     public class RecordsQueryResponse<T>
     {
         [JsonPropertyName("records")]
