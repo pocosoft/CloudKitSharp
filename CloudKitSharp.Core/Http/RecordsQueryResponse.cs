139d80780c34810789be2d32624bab8c328a1873
Akihiro Orikasa <a.orikasa@pocosoft.jp>
2022-09-05 23:28:35 +0900

    add docu comments

--- CloudKitSharp.Core/Http/RecordsQueryResponse.cs
+++ CloudKitSharp.Core/Http/RecordsQueryResponse.cs
@@ -3,8 +3,17 @@ using System.Text.Json.Serialization;
 
 namespace CloudKitSharp.Core.Http
 {
+    /// <summary>
+    /// Response
+    /// An array of dictionaries describing the results of the operation. The dictionary contains the following keys:
+    /// </summary>
+    /// <see cref="https://developer.apple.com/library/archive/documentation/DataManagement/Conceptual/CloudKitWebServicesReference/QueryingRecords.html#//apple_ref/doc/uid/TP40015240-CH5-SW4"/>
+    /// <typeparam name="T"></typeparam>
     public class RecordsQueryResponse<T>
     {
+        /// <summary>
+        /// An array containing a result dictionary for each record requested. If successful, the result dictionary contains the keys described in Record Dictionary. If unsuccessful, the result dictionary contains the keys described in Record Fetch Error Dictionary.
+        /// </summary>
         [JsonPropertyName("records")]
         public List<RecordDictionary<T>>? records { get; set; }
     }
--- CloudKitSharp.Core/Http/UserDiscoverRequest.cs
+++ CloudKitSharp.Core/Http/UserDiscoverRequest.cs
@@ -3,11 +3,13 @@
 namespace CloudKitSharp.Core.Http
 {
     /// <summary>
-    /// 
+    /// Discovering All User Identities (GET users/discover)
+    /// Fetches all user identities in the current user’s address book.
     /// </summary>
+    /// <see cref="https://developer.apple.com/library/archive/documentation/DataManagement/Conceptual/CloudKitWebServicesReference/DiscoveringAllUserIdentities.html#//apple_ref/doc/uid/TP40015240-CH31-SW1"/>
     public class UserDiscoverRequest : ICKRequest
     {
-        public Method Method => Method.Post;
+        public Method Method => Method.Get;
 
         public string SubPath => "/public/users/discover";
 
